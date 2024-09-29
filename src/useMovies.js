d8132e37a846d3d4e90f2b47473dc4ec1f088b9d
FaresGabsi <gabsi.fares18@gmail.com>
2024-09-29 20:14:36 +0100

    first commit

---
+++ src/useMovies.js
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react";
+const KEY = "9424263f";
+export function useMovies(query, callback) {
+  const [isLoadding, setIsLoadding] = useState(false);
+  const [movies, setMovies] = useState([]);
+  const [error, setError] = useState("");
+  useEffect(
+    function () {
+      callback?.();
+      const controller = new AbortController();
+      async function fetchMovies() {
+        try {
+          setIsLoadding(true);
+          setError("");
+          const res = await fetch(
+            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+            { signal: controller.signal }
+          );
+          if (!res.ok)
+            throw new Error("Something went wrong with fetching movies");
+          const data = await res.json();
+          if (data.Response === "False") throw new Error("Movie Not Found");
+          setMovies(data.Search);
+          console.log(data.Search);
+          setError("");
+        } catch (err) {
+          if (err.name !== "AbortError") setError(err.message);
+        } finally {
+          setIsLoadding(false);
+        }
+      }
+
+      if (query.length < 3) {
+        setMovies([]);
+        setError("");
+        return;
+      }
+      fetchMovies();
+      return function () {
+        controller.abort();
+      };
+    },
+    [query]
+  );
+  return { movies, error, isLoadding };
+}
